e36b0e2080613de854fc9eaf66c7831c09a8b455
deepthi <deepthibhogaraju.3@gmail.com>
2021-08-19 00:21:52 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
+	constructor(props) {
 		super(props);
+		this.state = {
+			pHidden: true
+		};
 	};
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+	render() {
+		return (
+			<div id="main">
+				<button id="click" onClick={() => {
+					this.setState({ pHidden: false })
+				}}>Show Para</button>
+				{
+					this.state.pHidden ? null : (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+
+				}
+			</div>
+		);
+	}
 }
 
 
